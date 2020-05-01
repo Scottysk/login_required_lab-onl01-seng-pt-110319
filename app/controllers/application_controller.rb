7bbc2d13ef846b86d763a36ffa8f1ffd794d444e
Scott Kwasnicki <kwasnickisw@mail.irsc.edu>
2020-05-01 02:00:00 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
   def current_user
-    @user = user
+    @user = User.find(params[:id])
   end
   
 end
